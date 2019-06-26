2c59af6bd90935dcc0b83d9189b39a2928793210
3194libin <15764747737@qq.com>
2019-06-26 14:46:28 +0800

    更改背景色

--- TKinter2.py
+++ TKinter2.py
@@ -5,7 +5,7 @@ class   APP:
         frame = tk.Frame(master)
         frame.pack(side = tk.LEFT,padx = 10,pady = 10)
 
-        self.hi_there = tk.Button(frame,text='打招呼',fg = 'blue',command = self.say_hi)
+        self.hi_there = tk.Button(frame,text='打招呼',bg = 'black',fg = 'white',command = self.say_hi)
         self.hi_there.pack()
     def say_hi(self):
         print('hello,我爱python')
